ee7dbf9a0854e327b1e2de7d8a306d761e1309dc
Thomas Jackson <jacksontj.89@gmail.com>
2014-03-05 18:58:22 -0800

    More whitespace conversion

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ from greenery.lego import parse
 regexes = sys.argv[1:]
 
 if len(regexes) < 2:
-	print("Please supply several regexes to compute their intersection, union and concatenation.")
-	print("E.g. \"19.*\" \"\\d{4}-\\d{2}-\\d{2}\"")
+    print("Please supply several regexes to compute their intersection, union and concatenation.")
+    print("E.g. \"19.*\" \"\\d{4}-\\d{2}-\\d{2}\"")
 
 else:
-	p = parse(regexes[0])
-	for regex in regexes[1:]:
-		p &= parse(regex)
-	print("Intersection:  %s" % ( p ))
+    p = parse(regexes[0])
+    for regex in regexes[1:]:
+        p &= parse(regex)
+    print("Intersection:  %s" % ( p ))
 
-	p = parse(regexes[0])
-	for regex in regexes[1:]:
-		p |= parse(regex)
-	print("Union:         %s" % ( p ))
+    p = parse(regexes[0])
+    for regex in regexes[1:]:
+        p |= parse(regex)
+    print("Union:         %s" % ( p ))
 
-	p = parse(regexes[0])
-	for regex in regexes[1:]:
-		p += parse(regex)
-	print("Concatenation: %s" % ( p ))
+    p = parse(regexes[0])
+    for regex in regexes[1:]:
+        p += parse(regex)
+    print("Concatenation: %s" % ( p ))
 
